8024ebe6f677f003873c2c042b89f53cd5875c3e
Joe Jordan <dev@ignitesoft.com>
2023-04-28 13:01:14 -0400

    deploy: stub deploy script

--- script/PancakeSwapSmartRouter.s.sol
+++ script/PancakeSwapSmartRouter.s.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import { PancakeSwapSmartRouter } from "src/PancakeSwapSmartRouter.sol";
 
-contract CounterScript is Script {
+contract PancakeSwapSmartRouterScript is Script {
     function setUp() public {}
 
     function run() public {

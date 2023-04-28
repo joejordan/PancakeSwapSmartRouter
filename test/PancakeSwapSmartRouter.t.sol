72be204ea2a4ea4d0c738003ccb1edc6f4d3e3b9
Joe Jordan <dev@ignitesoft.com>
2023-04-28 13:00:06 -0400

    test: stub test

---
+++ test/PancakeSwapSmartRouter.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/PancakeSwapSmartRouter.sol";
+
+contract PancakeSwapSmartRouterTest is Test {
+    PancakeSwapSmartRouter public router;
+
+    function setUp() public {
+        // router = new PancakeSwapSmartRouter();
+    }
+}
